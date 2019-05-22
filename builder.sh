8dd44f8fc18b6847e4935b2aafdec31ae9b8e6b7
tsygankov <tsygankov@magora-systems.com>
2019-05-22 16:05:43 +0700

    correct repository

--- builder.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-eval gulp --config=./config-index.json --IS_MAIN_PAGE=true build
-
--- gulp/webServer.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(gulp, options, config, wrapPipe) {
-
-    var webServer = require('gulp-webserver');
-
-    return gulp.task('webServer', wrapPipe(function (success, error) {
-        gulp.src(config['public'])
-            .pipe(webServer(options['server']));
-    }));
-};
